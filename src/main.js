8c307c60ad038d7bca3f77ccfda4258774db8dbe
zhiwarr <zhiwar04@gmail.com>
2024-10-18 18:44:32 +0300

    main js changes

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-import { registerSW } from "virtual:pwa-register";
+// import { registerSW } from "virtual:pwa-register";
 import PrimeVue from "primevue/config";
 
 import "./assets/base.css";
 import "./assets/main.css";
 
-registerSW({ immediate: true });
+// registerSW({ immediate: true });
 const app = createApp(App);
 app.use(PrimeVue);
 
