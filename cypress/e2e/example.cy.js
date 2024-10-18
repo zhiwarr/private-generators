6c66d703486d769a32484e9930e9cb51e914df46
zhiwarr <zhiwar04@gmail.com>
2024-10-18 18:51:27 +0300

    some changes

--- cypress/e2e/example.cy.js
+++
@@ -1,8 +0,0 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe("Sanity Test", () => {
-  it("visits the app root url", () => {
-    cy.visit("/");
-    cy.contains("#header a:first-child", "Music");
-  });
-});
