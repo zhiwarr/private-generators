50c1712f574b22b78822091c4a37129133adbd7f
zhiwarr <zhiwar04@gmail.com>
2024-10-18 18:34:11 +0300

    url changed

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/private-generator/",
+  base: "/private-generators/",
 
   plugins: [
     vue(),
